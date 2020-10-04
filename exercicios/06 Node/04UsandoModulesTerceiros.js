f982cc815c4eac56f8c11371c75e292ec1012c0c
jorge luiz <jorgeluizti@hotmail.com>
2020-10-04 13:34:11 -0300

    atualizar

--- exercicios/06 Node/04UsandoModulesTerceiros.js
+++ exercicios/06 Node/04UsandoModulesTerceiros.js
@@ -1,3 +1,2 @@
 const _ = require('lodash')
-setInterval(() => console.log(_.random(1, 10)), 2000) 
-
+setInterval(() => console.log(_.random(5, 10)), 2000) 
--- exercicios/06 Node/pastaB/acessarModulos.js
+++ exercicios/06 Node/pastaB/acessarModulos.js
@@ -1,2 +1,11 @@
 const modulaA = require('../01moduloa')
 console.log(modulaA.ola)
+
+const c = require('../pasteC')
+console.log(c.ola2)
+
+/*const http = require('http')
+http.createServer((req, res) => {
+    res.write('Bom dia')
+    res.end()
+}),listen(8080)*/
