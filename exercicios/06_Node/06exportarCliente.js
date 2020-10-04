9eafb6546c73a0defefff550ebc00c68bc2009cd
jorge luiz <jorgeluizti@hotmail.com>
2020-10-04 16:41:06 -0300

    Atualização

---
+++ exercicios/06_Node/06exportarCliente.js
@@ -0,0 +1,2 @@
+const exportar = require('./05exportar')
+console.log(exportar)
---
+++ exercicios/06_Node/funcionarios/funcionarios.js
@@ -0,0 +1,25 @@
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios')
+
+//1 - função filtrar o país 
+const chineses = f => f.pais === 'China'
+//2 - função filtrar mulheres chinesas
+const mulheres = f => f.genero ==='F'
+//3 - função filtrar o menor salário
+const menorSalario = (func, funcAtual) => {
+    return (func.salario < funcAtual.salario) ? func : funcAtual
+}
+
+axios.get(url).then(response =>{
+    const funcionarios = response.data
+    //console.log(funcionarios)
+
+    // mulher chinesa com menor salário
+    const func = funcionarios
+    .filter(chineses)
+    .filter(mulheres)
+    .reduce(menorSalario)
+
+    console.log(func)
+
+})
